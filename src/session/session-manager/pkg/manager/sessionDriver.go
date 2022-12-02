bb78b4891cd83c65f231531c4057a5dd306b0860
Shawn Horvatic <shorvatic@8thlight.com>
2022-12-02 15:29:01 -0600

    add basic pipes for dotnet

--- src/session/session-manager/pkg/manager/sessionDriver.go
+++ src/session/session-manager/pkg/manager/sessionDriver.go
@@ -9,7 +9,15 @@ func RunSession() {
 	
 	sessionOutPipe, fileInPipe := BuildPipes()
 
-	line, _ := sessionOutPipe.ReadBytes('\n')
+	line, err := sessionOutPipe.ReadBytes('\n')
 	fmt.Print("Message from File Manager: " + string(line))
 	fileInPipe.WriteString("Hello From Session Manager\n")
+	for {
+		line, err = sessionOutPipe.ReadBytes('\n')
+		if err == nil {
+			fmt.Println("Waitng for Web API")
+			fmt.Print("Message from Web Api: " + string(line))
+			fmt.Println("Hit for Web Hit")
+		}
+	}
 }
--- src/web-interface/Share.Web/Program.cs
+++ src/web-interface/Share.Web/Program.cs
@@ -1,6 +1,15 @@
+using System.IO.Pipes;
+using System.Security.Principal;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", async () => {
+    using FileStream fs = File.OpenWrite("/tmp/sessionInPipe");
+    var message = Encoding.ASCII.GetBytes("Hello From Dotnet\n");
+    await fs.WriteAsync(message, 0, message.Length);
+});
 
 app.Run();
+
